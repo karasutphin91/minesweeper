d68be24953924aa3398ac44e6fc697a46ba4a538
Kara Sutphin <kara.sutphin@ozmo-mac-2084.local>
2023-07-31 11:37:53 -0400

    bg color onclick

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -4,16 +4,27 @@ export const StyledCell = styled.div`
   min-width: 3rem;
   min-height: 3rem;
   border: 1px solid purple;
-  background-color: pink;
+  &.initial {
+    background-color: pink;
+  }
+  &.clicked {
+    background-color: purple;
+  }
+
 `;
+function clickMe(label: number) {
+  const e = document.getElementsByClassName("initial");
+  e[label].classList.add("clicked");
+}
 
 interface Props {
   label: number;
 }
 
 const Cell = ({label}: Props) => {
+
   return (
-    <StyledCell>
+    <StyledCell className="initial" onClick={() => clickMe(label)}>
       {label}
     </StyledCell>
   )
